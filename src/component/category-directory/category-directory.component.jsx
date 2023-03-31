d2b5844f21d2ca62b72c58d585fb12035bb22cd3
Paul Polster <paul.polster@speakap.nl>
2023-03-31 11:24:07 +0200

    components for directory and item

---
+++ src/component/category-directory/category-directory.component.jsx
@@ -0,0 +1,12 @@
+import './category-directory.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+const CategoryDirectory = ({ categories }) => {
+	return (
+		<div className="categories-container">
+			{categories.map((category) => <CategoryItem key={category.id} category={category} />)}
+		</div>
+	);
+};
+
+export default CategoryDirectory;
